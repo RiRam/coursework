53d8757b106064df817671414eaa62060cd46e21
Erica Ram <eram1294@gmail.com>
2016-07-07 13:25:59 -0400

    completed stream_stats exercise, updated gitignore

--- week4/stream_stats.py
+++ week4/stream_stats.py
@@ -6,6 +6,7 @@ import fileinput
 if __name__ == '__main__':
 
     d = {}
+    measures = {}
 
     def median(list):
         sortedList = sorted(list)
@@ -16,11 +17,12 @@ if __name__ == '__main__':
             return sortedList[index]
         else:
             return (sortedList[index] + sortedList[index + 1])/2.0
-    
+            
     def mean(list):
-        return float(sum(list))/float(len(list))
+        total = sum(list)
+        mean = total / float(len(list))
+        return mean
         
-
     # check for input filename given as first argument
     sys.argv = ['stream_stats.py', 'sample_input.tsv']
     if len(sys.argv) < 2:
@@ -31,20 +33,28 @@ if __name__ == '__main__':
     for line in fileinput.input():
         # split on tab to get the key and value for each line
         fields = line.rstrip('\n').split('\t')
+
+        k = fields[0]
+        v = float(fields[1])        
         
         # update statistics for the group correponding to this key
         # (minimum, median, mean, and maximum)
-        if d.has_key(fields[0]):
-            d[fields[0]].append(int(fields[1]))
-            d[fields[0]+ ' stats'] = [int(fields[1]), int(fields[1]), int(fields[1]), int(fields[1])]
+        if k in d:
+            d[k].append(v)
+            measures[k] = [min(d[k]), median(d[k]), mean(d[k]), max(d[k])]
         else:
-            d[fields[0]] = []
-            d[fields[0]].append(int(fields[1]))
-            d[fields[0]+ ' stats'] = [min(d[fields[0]]), median(d[fields[0]]), mean(d[fields[0]]), max(d[fields[0]])]
+            d[k] = []
+            d[k].append(v)
+            measures[k] = [v, v, v, v]
             
             
     fileinput.close()
-        
-        
-
+    
     # loop over each group and output the group's key and statistics
+    filewriter = open("output.tsv", "w")
+    
+    for k in d: 
+        string = k + "\t" + '\t'.join([str(x) for x in measures[k]]) + "\n"
+        filewriter.write(string)
+        
+    filewriter.close()    
